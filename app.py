ecd05913a76401c3a7bbe02cf97a15bbd8b82c3a
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-03-21 17:22:03 +0000

    Initial project setup

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
+
+@app.route('/genres')
+def genres():
+    return render_template('genres.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return "Thank you for your message!"
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
