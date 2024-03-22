732020441522d61ccd13bd1177d032983d03c53e
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-03-22 22:52:22 +0000

    Update app.py
    changed games route to shop, added buy route which depletes stock based on the "buy now" button click and added buy() function which searches the inventory list for an associated id - if stock is available it decrements stock by 1 and returns a successful purchase.

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
+class GameItem:
+    def __init__(self, id, name, price, stock, image_url):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.image_url = image_url
+
+inventory = [
+    GameItem(1, "Runescape T-Shirt", 50, 10, 'images/runescapet.webp'),
+    GameItem(2, "The Witcher Tea Mug", 60, 5, "images/witchermug.jpg"),
+    GameItem(3, "Kirby Flask", 70, 20, "images/kirbyflask.webp"),
+    GameItem(4, "Super Mario T-Shirt", 80, 0, "images/marioshirt.jpg")  
+]
+
+@app.route('/buy/<int:item_id>', methods=['GET', 'POST'])
+def buy(item_id):
+    item = next((item for item in inventory if item.id == item_id), None)
+    if item and item.stock > 0:
+        item.stock -= 1
+        message = f"Successfully purchased {item.name}."
+    else:
+        message = "This item is out of stock."
+    
+    
+
+    return message
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/games')
-def games():
-    return render_template('games.html')
-
+@app.route('/shop')
+def shop():
+    return render_template('shop.html', inventory=inventory)
+  
 @app.route('/genres')
 def genres():
     return render_template('genres.html')
